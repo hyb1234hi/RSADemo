88e085ccc04a1664c6beb2ceeacf53fea96b275c
sunxuelin <sunxuelin@uhope.com>
2018-07-06 15:30:03 +0800

    RSA简单应用

---
+++ RSADemo/RSA/RSAEncryptor.h
@@ -0,0 +1,48 @@
+//
+//  RSAEncryptor.h
+//  RSADemo
+//
+//  Created by Sunny on 2018/6/6.
+//  Copyright © 2018年 sunny. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RSAEncryptor : NSObject
+
+/**
+ * rsa加密方法
+ *
+ * @param str  需要加密的字符串
+ * @param  path  ‘.der'格式的公钥文件路径
+ */
+
++ (NSString *)encryptString:(NSString *)str publicKeyWithContentsOfFile:(NSString *)path;
+
+/**
+ * rsa解密方法
+ *
+ * @param str  需要解密的字符串
+ * @param  path  ‘.p12'格式的私钥文件路径
+ * @param  password  私钥文件密码
+ */
++ (NSString *)decryptString:(NSString *)str privateKeyWithContentsOfFile:(NSString *)path password:(NSString *)password;
+
+/**
+ * rsa加密方法
+ *
+ * @param str  需要加密的字符串
+ * @param  pubKey  公钥字符串
+ */
+
++ (NSString *)encryptString:(NSString *)str publicKey:(NSString *)pubKey;
+
+/**
+ *  解密方法
+ *
+ *  @param str     需要解密的字符串
+ *  @param privKey 私钥字符串
+ */
++ (NSString *)decryptString:(NSString *)str privateKey:(NSString *)privKey;
+
+@end
--- RSADemo/ViewController.m
+++ RSADemo/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "RSAEncryptor.h"
 
 @interface ViewController ()
+
 
 @end
 
@@ -16,9 +18,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+   
+    NSString *originalString = @"这是一段将要使用'.der'文件加密的字符串!";
+    NSLog(@"加密前:%@", originalString);
+   
+    
+     //使用.der和.p12中的公钥私钥加密解密
+    ///公钥加密
+    NSString *public_key_path = [[NSBundle mainBundle] pathForResource:@"public_key.der" ofType:nil];
+    NSString *encryptStr = [RSAEncryptor encryptString:originalString publicKeyWithContentsOfFile:public_key_path];
+    NSLog(@"加密后:%@", encryptStr);
+    
+    ///私钥解密，私钥文件密码是123456
+    NSString *private_key_path = [[NSBundle mainBundle] pathForResource:@"private_key.p12" ofType:nil];
+    NSString *decryptStr = [RSAEncryptor decryptString:encryptStr privateKeyWithContentsOfFile:private_key_path password:@"123456"];
+    NSLog(@"解密后:%@", decryptStr);
 
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
